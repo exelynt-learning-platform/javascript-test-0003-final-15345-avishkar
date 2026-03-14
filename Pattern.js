386ded49a60a2641096fc5a226d8ad7a7c4223c7
Mr. Avishkar Kesarkar <avi.ak.2611@gmail.com>
2026-03-14 11:52:08 +0530

    Update Pattern.js
    Solved alternating 1-0 pattern using nested loops

--- Pattern.js
+++ Pattern.js
@@ -1,1 +1,18 @@
+let rows = 6;
 
+for (let i = 1; i <= rows; i++) {
+
+    let value = i % 2 === 1 ? 1 : 0;
+
+    let line = "";
+
+    for (let j = 1; j <= i; j++) {
+
+        line += value + " ";
+
+        value = value === 1 ? 0 : 1;
+
+    }
+
+    console.log(line);
+}
